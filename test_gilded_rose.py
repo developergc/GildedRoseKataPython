61df16cd5558dcb27427f72aa757004eded698c7
G C <gc@Gs-MacBook-Pro.local>
2024-10-01 23:54:43 -0700

    lab4
    lab4

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -9,8 +9,36 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
 
+    
+    def test_backstage(self):
+        bp = "Backstage passes to a TAFKAL80ETC concert"
+        items = [Item(bp, 0, 10)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        ans = 5
+        assert items[0].quality == ans
+
+
+    def test_sulfuras(self):
+        s = "Sulfuras, Hand of Ragnaros"
+        items = [Item(s, 0, 20)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        assert items[0].quality == 18
+        assert items[0].sell_in == 1
+
+    def test_aged_brie(self):
+        ab = "Aged Brie"
+        items = [Item(ab, 2, 48)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        gr.update_quality()
+        gr.update_quality()
+        gr.update_quality()
+        ans = 52
+        assert items[0].quality == ans
 
 if __name__ == '__main__':
     unittest.main()
